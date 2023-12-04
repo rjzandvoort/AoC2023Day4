dd59d254fca7d03dab95b398a279a80d29456b4c
Robert-Jan Zandvoort <rjzandvoort@gmail.com>
2023-12-04 07:16:35 +0100

    Add project files.

---
+++ Day4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day4b();
+        }
+
+        static void Day4a()
+        {
+            var txt = File.ReadAllText("input.txt");
+            long totalPoints = 0;
+            txt = txt.Replace("\r", "");
+            // Make an array
+            foreach (var lin in txt.Split('\n'))
+            {
+                var lps = lin.Split(':')[1].Split('|');
+                var wnrs = lps[0].Trim().Replace("  "," ").Split(' ').ToList().Select(an => int.Parse(an)).ToList();
+                var mnrs = lps[1].Trim().Replace("  ", " ").Split(' ').ToList().Select(an => int.Parse(an)).ToList();
+
+                var nrWin = mnrs.Count(m => wnrs.Contains(m));
+                double points = 0;
+                if(nrWin > 0)
+                    points = Math.Pow(2, nrWin-1);
+                totalPoints += (long) points;
+            }
+
+            Console.WriteLine("Points : " + totalPoints);
+            Console.ReadLine();
+        }
+
+        static void Day4b()
+        {
+            var txt = File.ReadAllText("input.txt");            
+            txt = txt.Replace("\r", "");
+            var allCards = new Dictionary<int, long>();
+            var allLines = txt.Split('\n');
+            for (int i = 1; i <= allLines.Length; i++) allCards.Add(i, 1);
+            // Make an array
+            int cardNr = 1;
+            foreach (var lin in allLines)
+            {
+                var lps = lin.Split(':')[1].Split('|');
+                var wnrs = lps[0].Trim().Replace("  ", " ").Split(' ').ToList().Select(an => int.Parse(an)).ToList();
+                var mnrs = lps[1].Trim().Replace("  ", " ").Split(' ').ToList().Select(an => int.Parse(an)).ToList();
+
+                var nrWin = mnrs.Count(m => wnrs.Contains(m));
+                
+                for(int i=cardNr+1;i<=cardNr+nrWin && i<=allLines.Length;i++)
+                {
+                    allCards[i] += 1 * allCards[cardNr];
+                }
+                cardNr++;
+            }
+
+            long totalCards = allCards.Sum(c => c.Value);
+            Console.WriteLine("Total Cards : " + totalCards);
+            Console.ReadLine();
+        }
+    }
+}
